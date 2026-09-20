213c17e0cae11b141fc879a7fd3867b98b3d3c5b
WMJR07 <20233029163@estudantes.ifpr.edu.br>
2026-09-20 17:37:41 -0300

    commit inicial da arquitetura do projeto

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import DefaultLayout from "interface/DefaultLayout";
+
+function Home() {
+  return (
+    <DefaultLayout
+      metadata={{
+        description:
+          "Conduzindo Para O Futuro | Buscando conectar instrutores a alunos que tem interesse em ter aulas de direção.",
+      }}
+    >
+      <h1>
+        🚙 Conduzindo Para O Futuro 🛵
+      </h1>
+      <h3>
+        🚌 Buscando conectar instrutores a alunos que tem interesse em ter aulas de direção! 🚚
+      </h3>
+    </DefaultLayout>
+  );
+}
+
+export default Home;
